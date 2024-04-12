5e28d375172d995246c7bcdeeb2aafc66b24ccb4
Fhellype Vinicius de Paula <108161601+Madarinha-dev@users.noreply.github.com>
2024-04-12 18:30:21 -0300

    .

--- dados.py
+++ dados.py
@@ -1,20 +1,13 @@
 from flask import Flask, request, render_template, jsonify, redirect
 app = Flask(__name__)
 
+@app.route('/acesso', methods=['POST'])
+def acessoaluno():
+    sala = request.form.get('sala')
+    senha = request.form.get('senha')
 
-@app.route('/dados.py', methods=['POST'])
-def processar_formulario():
-    sala = request.form['sala']
-    senha = request.form['senha']
-    if sala == 'adm':
-        if senha == 3:
-            return render_template('index.html')
-        # eu quero levar pra tela index so pra teste
-        else:
-            print('senha errada (teste)')
-    else:
-        print('deu errado (teste)')
+    print(f'A sala:',{sala})
+    print(f'A senha:', {senha})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return redirect('/desafios')
 
